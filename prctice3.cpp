fa64d40b1ef73a303082ede630fd585219779640
Ali Azaz <153861266+aliazaz001@users.noreply.github.com>
2024-01-06 22:49:57 +0600

    Add files via upload

---
+++ prctice3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Please enter first number =";
+    cin>>a;
+    cout<<"please enter the second number =";
+    cin>>b;
+
+    int sum ;
+    sum=a+b;
+
+
+
+    cout<<"The answer is ="<<sum;
+    return 0;
+}
